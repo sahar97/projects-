391538c9891bf19cd66df21dfa3365ff85640f30
double click <double click@doubleclick-PC>
2018-01-21 18:56:18 +0200

    test7

---
+++ box1/src/box1.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class box1 {
+
+	public static void main(String[] args) {
+	
+		JOptionPane.showMessageDialog(null, "welcome to java");
+		System.exit(0);
+	}
+
+}
